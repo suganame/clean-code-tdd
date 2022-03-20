fc6991f71650cf9a742f4705fe16ec0fb7a608da
suganame <gsuganame@gmail.com>
2022-03-20 14:01:44 -0300

    refactor: make sut creation to a factory method

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -19,8 +19,8 @@ describe('Signup Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('name'))
   })
-  test('Should return 400 if no name is provided ', () => {
-    const sut = new SignupController()
+  test('Should return 400 if no email is provided ', () => {
+    const sut = makeSut()
     const httpRequest = {
       body: {
         name: 'Guilherme Suganame',
@@ -33,8 +33,8 @@ describe('Signup Controller', () => {
     expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
 
-  test('Should return 400 if no name is provided ', () => {
-    const sut = new SignupController()
+  test('Should return 400 if no password is provided ', () => {
+    const sut = makeSut()
     const httpRequest = {
       body: {
         name: 'Guilherme Suganame',
@@ -47,8 +47,8 @@ describe('Signup Controller', () => {
     expect(httpResponse.body).toEqual(new MissingParamError('password'))
   })
 
-  test('Should return 400 if no name is provided ', () => {
-    const sut = new SignupController()
+  test('Should return 400 if no passwordConfirmation is provided ', () => {
+    const sut = makeSut()
     const httpRequest = {
       body: {
         name: 'Guilherme Suganame',
